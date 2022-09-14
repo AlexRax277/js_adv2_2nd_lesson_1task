cc07de36a061c59ea35832c142b46794f0a446f2
Александр Раковчен <italianhorse20@gmail.com>
2022-09-14 18:50:08 +0300

    changing public-path

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: `${__dirname}/src/index.js`,
   output: {
     path: `${__dirname}/dist`,
-    publicPath: '/dist',
+    publicPath: '/',
     // path: path.resolve(__dirname, 'dist'),
     // publicPath: '/',
   },
